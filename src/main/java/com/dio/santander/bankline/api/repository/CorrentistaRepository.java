eb5efea14b69f430128caf5141d4cfb1937a0ca5
Ricardo Bruno <75039505+RicardoBrunoB@users.noreply.github.com>
2022-05-04 16:42:50 -0300

    Update CorrentistaRepository

--- src/main/java/com/dio/santander/bankline/api/repository/CorrentistaRepository.java
+++ src/main/java/com/dio/santander/bankline/api/repository/CorrentistaRepository.java
@@ -1,6 +1,6 @@
 package com.dio.santander.bankline.api.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository; 
 
 import com.dio.santander.bankline.api.model.Correntista;
 
